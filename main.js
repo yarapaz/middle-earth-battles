41eda631f0d1db226cee18bb5cc05a0d8311a4aa
Yara <yara.gordillo@gmail.com>
2022-10-31 10:04:34 +0100

    first js requirements fulfilled

--- main.js
+++ main.js
@@ -6,21 +6,28 @@ const select = document.querySelector('.js_select');
 const battleText = document.querySelector('.js_battle_text');
 
 //Functions
-
 function randomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function handleClick() {
-  const selectValue = parseInt(select.value);
-  if (selectValue > randomNumber(6)) {
+function battleMessage() {
+  let selectValue = parseInt(select.value);
+  if (selectValue < randomNumber(6)) {
+    battleText.classList.add('bigger');
     battleText.innerHTML =
       '¡Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
-  } else if (selectValue < randomNumber(6)) {
+  } else if (selectValue > randomNumber(6)) {
+    battleText.classList.remove('bigger');
     battleText.innerHTML = '¡Ha ganado el Ejército del Bien! Enhorabuena.';
   } else if (selectValue === randomNumber(6)) {
+    battleText.classList.remove('bigger');
     battleText.innerHTML = 'Empate';
   }
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  battleMessage();
 }
 
 //Events
