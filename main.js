8ac5687ac1d6e8391802b572ec63c4697013df19
Yara <yara.gordillo@gmail.com>
2022-10-31 10:57:00 +0100

    finishing js bonus

--- main.js
+++ main.js
@@ -6,36 +6,45 @@ const select = document.querySelector('.js_select');
 const battleText = document.querySelector('.js_battle_text');
 const userCounter = document.querySelector('.js_user_counter');
 const computerCounter = document.querySelector('.js_computer_counter');
+let counter = 0;
 
 //Functions
 function randomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+function compare(user, computer) {
+  if (user > computer) {
+    battleText.innerHTML =
+      '¡Has ganado el juego! Ahora la Tierra Media podrá vivir en paz';
+    btn.innerHTML = 'Reiniciar Juego';
+  } else if (user < computer) {
+    battleText.innerHTML =
+      'Has perdido el juego. Las sombras se apoderarán de las tierras de los hombres libres';
+    btn.innerHTML = 'Reiniciar Juego';
+  } else if (user === computer) {
+    battleText.innerHTML =
+      '¡Habéis empatado! Todo se decidirá en la siguiente batalla';
+    btn.innerHTML = 'Reiniciar Juego';
+  }
+}
+
 function battleMessage() {
   let selectValue = parseInt(select.value);
-  let counter = 0;
   const counterMaxLimit = 10;
-  counter++;
   if (selectValue < randomNumber(5) && counter < counterMaxLimit) {
+    counter = counter + 1;
+    computerCounter.innerHTML = counter;
     battleText.innerHTML =
       '¡Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
-    computerCounter.innerHTML = `Ordenador:  ${counter}`;
   } else if (selectValue > randomNumber(5) && counter < counterMaxLimit) {
+    counter = counter + 1;
+    userCounter.innerHTML = counter;
     battleText.innerHTML = '¡Ha ganado el Ejército del Bien! Enhorabuena.';
-    userCounter.innerHTML = `Jugadora:  ${counter}`;
   } else if (selectValue === randomNumber(5) && counter < counterMaxLimit) {
     battleText.innerHTML = 'Empate';
   } else if (counter === counterMaxLimit) {
-    if (userCounter > computerCounter) {
-      battleText.innerHTML =
-        '¡Has ganado el juego! Ahora la Tierra Media podrá vivir en paz';
-      btn.innerHTML = 'Reiniciar Juego';
-    } else {
-      battleText.innerHTML =
-        'Has perdido el juego. Las sombras se apoderarán de las tierras de los hombres libres';
-      btn.innerHTML = 'Reiniciar Juego';
-    }
+    compare(userCounter, computerCounter);
   }
 }
 
