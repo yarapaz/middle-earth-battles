99ee34371e4ba2d8de6d9923fa33996b58dc605b
Yara <yara.gordillo@gmail.com>
2022-10-31 12:59:07 +0100

    final game improved with everything reviewed in class

--- main.js
+++ main.js
@@ -1,71 +1,121 @@
 'use strict';
 
 //DOM elements to retrieve
-const btn = document.querySelector('.js_btn');
+const battleBtn = document.querySelector('.js_battle_btn');
+const resetBtn = document.querySelector('.js_reset_btn');
 const select = document.querySelector('.js_select');
 const battleText = document.querySelector('.js_battle_text');
 const userCounterText = document.querySelector('.js_user_counter');
 const computerCounterText = document.querySelector('.js_computer_counter');
 let userCounter = 0;
 let computerCounter = 0;
+let moves = 0;
 
 //Functions
-function randomNumber(max) {
+function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function compare(user, computer) {
-  if (user > computer) {
+function getComputerRacePP() {
+  const randomNumber = getRandomNumber(5);
+  let result = 0;
+  if (randomNumber === 1 || randomNumber === 2 || randomNumber === 3) {
+    result = 2;
+  } else if (randomNumber === 4) {
+    result = 3;
+  } else {
+    result = 5;
+  }
+
+  return result;
+}
+
+function getUserRacePP() {
+  return parseInt(select.value);
+}
+
+function handleResetClick(event) {
+  event.preventDefault();
+  restartGame();
+  userCounter = 0;
+  computerCounter = 0;
+  moves = 0;
+  userCounterText.innerHTML = userCounter;
+  computerCounterText.innerHTML = computerCounter;
+  battleText.innerHTML = '¡Comienza la batalla!';
+}
+
+function compareCounter(userCounter, computerCounter) {
+  if (userCounter > computerCounter) {
     battleText.innerHTML =
       '¡Has ganado el juego! Ahora la Tierra Media podrá vivir en paz';
-    btn.innerHTML = 'Reiniciar Juego';
-  } else if (user < computer) {
+  } else if (userCounter < computerCounter) {
     battleText.innerHTML =
       'Has perdido el juego. Las sombras se apoderarán de las tierras de los hombres libres';
-    btn.innerHTML = 'Reiniciar Juego';
-  } else if (user === computer) {
+  } else if (userCounter === computerCounter) {
     battleText.innerHTML =
       '¡Habéis empatado! Todo se decidirá en la siguiente batalla';
-    btn.innerHTML = 'Reiniciar Juego';
   }
 }
 
-function battleMessage() {
-  let selectValue = parseInt(select.value);
-  const counterMaxLimit = 10;
+function renderScore() {
+  userCounterText.innerHTML = userCounter;
+  computerCounterText.innerHTML = computerCounter;
+}
+
+function gameOver() {
+  battleBtn.classList.add('hidden');
+  resetBtn.classList.remove('hidden');
+}
+
+function restartGame() {
+  battleBtn.classList.remove('hidden');
+  resetBtn.classList.add('hidden');
+}
+
+function comparePP(userRacePP, computerRacePP) {
+  const maxMoves = 10;
+  moves++;
   if (
-    selectValue < randomNumber(5) &&
-    computerCounter < counterMaxLimit &&
-    userCounter < counterMaxLimit
+    userRacePP < computerRacePP &&
+    computerCounter < maxMoves &&
+    userCounter < maxMoves
   ) {
-    computerCounter = computerCounter + 1;
+    computerCounter++;
     computerCounterText.innerHTML = computerCounter;
     battleText.innerHTML =
       '¡Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
   } else if (
-    selectValue > randomNumber(5) &&
-    userCounter < counterMaxLimit &&
-    computerCounter < counterMaxLimit
+    userRacePP > computerRacePP &&
+    userCounter < maxMoves &&
+    computerCounter < maxMoves
   ) {
-    userCounter = userCounter + 1;
+    userCounter++;
     userCounterText.innerHTML = userCounter;
     battleText.innerHTML = '¡Ha ganado el Ejército del Bien! Enhorabuena.';
   } else if (
-    selectValue === randomNumber(5) &&
-    computerCounter < counterMaxLimit &&
-    userCounter < counterMaxLimit
+    userRacePP === computerRacePP &&
+    computerCounter < maxMoves &&
+    userCounter < maxMoves
   ) {
     battleText.innerHTML = 'Empate';
   }
-  if (userCounter === counterMaxLimit || computerCounter === counterMaxLimit) {
-    compare(userCounter, computerCounter);
+  if (userCounter === maxMoves || computerCounter === maxMoves) {
+    compareCounter(userCounter, computerCounter);
+    gameOver();
   }
 }
 
-function handleClick(event) {
+function handleBattleClick(event) {
   event.preventDefault();
-  battleMessage();
+  //Obtener la raza de buenos
+  const userRacePP = getUserRacePP();
+  //Obtener la raza de malos
+  const computerRacePP = getComputerRacePP();
+  comparePP(userRacePP, computerRacePP);
+  renderScore();
 }
 
 //Events
-btn.addEventListener('click', handleClick);
+battleBtn.addEventListener('click', handleBattleClick);
+resetBtn.addEventListener('click', handleResetClick);
